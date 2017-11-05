c0caabe1a996b7740d2a71c2eb87ed80ad4e0dc3
Juan Antonio Barragan <jabarragann@unal.edu.co>
2017-11-05 14:55:36 -0500

    MaximazeIt challenge solved

--- Python/Itertools/04_MaximazeIt.py
+++ Python/Itertools/04_MaximazeIt.py
@@ -7,3 +7,36 @@ Created on Sat Oct 14 17:40:13 2017
 
 """
 
+from itertools import product
+
+file=open("04_MaximazeIt3.txt","r")
+
+argArray= file.readline().split(" ")
+
+numberOfLines=int(argArray[0])
+modNumber=int(argArray[1])
+
+arrays=[]
+for i in range(numberOfLines):
+    argArray=file.readline().split(" ")
+    temp=[]
+    for i in argArray:
+        temp.append(int(i))        
+    
+    arrays.append(temp)
+
+
+product1=list(product(*arrays))
+
+maxNumb=0
+for arg in product1:
+    sum1=0
+    for item in arg:
+        sum1=sum1+item**2
+        result=sum1%modNumber
+    if result > maxNumb:
+        maxNumb=result
+        
+print("Answer is: "+ str(maxNumb))
+
+file.close()
